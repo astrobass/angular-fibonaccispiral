9d33e82bb23c86eed807b1454ab5a4355305bca0
Corey Wendling <github@wtree.com>
2017-08-13 18:22:23 -0400

    Turned off autowatch and fixed bug in pattern matching

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/**/*.js: ['coverage'],
+      'app/**/*.js': ['coverage'],
       'app/**/!(*Spec).js': ['coverage']
     },
 
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
@@ -68,7 +68,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
