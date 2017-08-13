7370a2cca055e0e47e4aa441f54839b000d19cf5
Corey Wendling <github@wtree.com>
2017-08-13 18:28:15 -0400

    Updated pattern matching again

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'app/**/*.js': ['coverage'],
       'app/**/!(*Spec).js': ['coverage']
     },
 
