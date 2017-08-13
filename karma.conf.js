ddf59cd3300f0179779e3e632899ac14abaf2dcb
Corey Wendling <github@wtree.com>
2017-08-13 18:21:13 -0400

    Changed pattern matching for coverage

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '{app/**/*.js, app/**/!(*Spec).js}': ['coverage']
+      'app/**/*.js: ['coverage'],
+      'app/**/!(*Spec).js': ['coverage']
     },
 
 
